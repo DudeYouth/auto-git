d64026a43887bc891f21e7bfad365f561540fa00
YoungPigs <1157536217@qq.com>
2016-12-01 23:51:32 +0800

    testwdwadawdwddawdawwdwd

--- watch.py
+++ watch.py
@@ -10,6 +10,7 @@ from watchdog.events import FileSystemEventHandler
 
 global cacheTime
 global replace_reg
+global sign
 cacheTime=0
 parser=argparse.ArgumentParser()
 parser.add_argument('-s',type=str,help='commit的版本号标记字符，默认“:::”',default=':::')
@@ -29,18 +30,26 @@ class EventHandler(FileSystemEventHandler):
 def action(data):
     global cacheTime
     global replace_reg
-    commit=int(time.time())
-    if commit-int(cacheTime)>2:
-        replaceStr(data)
+    global sign
+    if int(time.time())-int(cacheTime)>2:
+        commit=replaceStr(data)
         time.sleep(1)
         os.system('git pull origin master')
         os.system('git add .')
-        os.system('git commit -m"'+str(commit).replace(sign,'')+'"')
+        os.system('git commit -m"'+commit.replace(sign,'')+'"')
         os.system('git push origin master')
         cacheTime=time.time()
 def replaceStr(data):
+    global sign
+    strings=str(time.time())
     for line in fileinput.input(data.src_path,inplace=1):
-        print(line.replace(sign,''))
+        strs=line.replace(sign,'');
+        if(line!=strs):
+            strings=strs
+            print('')
+        else:
+            print(line)
+    return strings
 if __name__=='__main__':
     ev=EventHandler()
     observer=Observer()
