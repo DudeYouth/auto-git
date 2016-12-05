a6eaa3ba541416a3641562db1c5ff6b61537eaf5
YoungPigs <1157536217@qq.com>
2016-12-06 00:00:10 +0800

    1480953557.064966

--- watch.py
+++ watch.py
@@ -5,17 +5,18 @@ import re
 import argparse
 import threading
 import fileinput
+import random
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
 from watchdog.events import FileSystemEventHandler
 global cacheTime
-cacheTime=0
+cacheTime=1
 parser=argparse.ArgumentParser()
 lock=threading.Lock();
 parser.add_argument('-s',type=str,help='commit的版本号标记字符，默认“:::”',default=':::')
 args = parser.parse_args()
 sign=args.s;
-def gitCommit(commit,test):
+def gitCommit(commit):
     try:
         os.system('git pull origin master')
         os.system('git add .')
@@ -36,27 +37,33 @@ class EventHandler(FileSystemEventHandler):
         action(data)
 def action(data):
     global cacheTime
-    if int(time.time())-int(cacheTime)>2:
-        commit=replaceStr(data)
-        time.sleep(1)
-        th=threading.Thread(target=gitCommit,args=(commit,123))
-        th.start();
-        cacheTime=time.time()
+    try:
+        if cacheTime:
+            cacheTime=0
+            commit=replaceStr(data)
+            gitCommit(commit)
+            # time.sleep(1)
+            # th=threading.Thread(target=gitCommit,args=(commit,123))
+            # th.start();
+    except Exception as e:
+        print(e)
+    finally:
+        cacheTime=1
 def replaceStr(data):
+    lock.acquire()
     try:
         strings=str(time.time())
-        lock.acquire();
-        for line in fileinput.input(data.src_path,inplace=1,backup=''):
+        for line in fileinput.input(data.src_path,inplace=True,backup=''): 
             strs=line.replace(sign,'');
             if(line!=strs):
                 strings=strs
                 print('')
             else:
                 print(line)
-        fileinput.close()
     except:
         print("检测失败")
     finally:
+        fileinput.close()
         lock.release();
         return strings
 if __name__=='__main__':
